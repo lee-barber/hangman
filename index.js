72c9d28282587560dc02403269ef49cb18243225
Lee Barber <lfb11@alumni.psu.edu>
2018-02-13 20:06:16 -0500

    added click event, working on attempts

--- index.js
+++ index.js
@@ -1,64 +1,3 @@
-// PSEUDO CODE WITH OLD CODE AND COMMENTS MIXED IN
-
-// # Rules
-// 6 initial attempts to guess word correctly
-// Player wins if he/she guesses word before 6 attemps are used
-// Player loses when attempts == 0
-
-// # Definitions
-// array of words = wordbank
-
-// variable that contains an array of the game words to choose from 
-var wordChoices = ["nomadic", "indignant", "disparity", "androgynous", "pragmatic", "patronage", "dilapidated", "putative"]
-
-// array of letters = alphabet
-
-// array of each letter in the alphabet to choose from
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-]
-// attempts remaining = 6
-var attempts = 6
-
-// # Functionality
-// Game starts upon page load
-
-// # Display
-// Rules
-// Blank spaces
-// Alphabet
-// Number of attempts
-
-// As long as (attempts > 0){
-
-// Game is live...
-//  Display to user: "Guess a letter"
-//  User guesses letter by clicking letter in displayed alphabet
-//  if (letter exists word){
-//      for each instance of that letter in word{
-//          change blank space to guessed letter
-//          mark letter as used in the alphabet and make it unclickable
-//      }
-//  }
-//  elseif (letter does NOT exist in word){
-//          mark letter as used in the alphabet and make it unclickable
-//          deduct 1 from number of attempts remaining
-//  }
-    
-//  if (guessed word == current word from wordbank){
-//      Stop program / exit loop
-//      Display "Game is over. You win."
-//      Display exit options
-//  }
-// }
-// Otherwise{
-//  Stop program / exit loop
-//  Display "Game is over. You lose."
-//  Display exit options
-// }
-
-
-
 // PSEUDO CODE
 
 // # Rules
@@ -94,7 +33,7 @@ var attempts = 6
 //          mark letter as used in the alphabet and make it unclickable
 //          deduct 1 from number of attempts remaining
 //  }
-    
+
 //  if (guessed word == current word from wordbank){
 //      Stop program / exit loop
 //      Display "Game is over. You win."
@@ -107,52 +46,48 @@ var attempts = 6
 //  Display exit options
 // }
 
+// variable that contains an array of the game words to choose from 
+var wordChoices = ["nomadic", "indignant", "disparity", "androgynous", "pragmatic", "patronage", "dilapidated", "putative"]
+
+// array of letters = alphabet
+
+// array of each letter in the alphabet to choose from
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
+// attempts remaining = 6
+var attempts = 6
+
+var wordHints = ["hint1", "hint2", "hint3", "hint4", "hint5", "hint6", "hint7", "hint8"]
+
 document.getElementById("chances").innerHTML = attempts;
-// OLD CODE WITH COMMENTS
 
 // creates a for loop that starts at 0 and cycles through the alphabet length then adds 1
 for (let i = 0; i < alphabet.length; i++) {
-
-    // creates a new element named "div" for each letter in the alphabet loop
+    // creates a new element named "div" for each letter in the alphabet loop (a ghost div that isn't seen by the user)
     var div = document.createElement('div');
-
     // uses DOM to print to the HTML the alphabet array at its current index
     var letters = document.createTextNode(alphabet[i])
-
-    div.appendChild(letters);  
-
+    div.appendChild(letters);
     // adds the class "letterBox" to each of the divs that were created
     div.classList.add('letterBox');
-
     // as the loop cycles, a new div will be created (i.e. button1, button2, etc)
     div.id = "button" + i;
-
     // places the divs that were created within the body HTML element
     document.body.appendChild(div);
-
     // places all of the classes with 'letterBox' inside a variable called "box"
     var box = document.getElementById(div.id);
     // console.log(box)
-
-// creates an event listener that upon clicking...
+    // creates an event listener that upon clicking...
     box.addEventListener('click', function() {
-
-        document.getElementById('button' + i).style.backgroundColor = 'orange';
-        document.getElementById("chances").innerHTML = (attempts--);
-        console.log(alphabet[i])
-
-
-        checkWord(alphabet[i])
-        // checks to see if the letter is in the word
-
-
-
-        // shows the letter on the dotted line if it is correct
-        // showLetter(i)
-
-        // hides the letter after being chosen
-        // hideLetter(i)
-        
+          if (attempts >= 0) {
+            document.getElementById("chances").innerHTML = (attempts--);
+            document.getElementById('button' + i).style.backgroundColor = 'orange';
+            checkWord(alphabet[i])
+        }else {
+            alert('You lose!')
+            location.reload()
+        }
     })
 }
 
@@ -160,19 +95,14 @@ for (let i = 0; i < alphabet.length; i++) {
 
 // creates a function called "checkWord" that allows the variable "guessedLetter" to be passed as an argument
 function checkWord(guessedLetter) {
-
     // creates an if statement that if the variable "currentWord" includes the variable "guessedLetter" then...
     if (currentWord.includes(guessedLetter)) {
-
         // create an arbitrary variable that is assigned to the value of the variable "currentWord" at the index of the variable "guessedLetter"
         var n = currentWord.indexOf(guessedLetter);
-
         // prints the guessedLetter for debugging
         console.log(guessedLetter);
-
         // showLetter(guessedLetter)
         var dashLetter = document.getElementById("guessLine").textContent;
-
         // prints the variable dashLetter for debugging
         console.log(dashLetter);
 
@@ -191,9 +121,6 @@ function checkWord(guessedLetter) {
         // console.log(ltrArr);
         placeLetter(dashArray);
 
-        // appear on the dotted line
-
-        // remove letter from guessed word so that it doesn't check to that letter again
     } else {
         console.log('incorrect')
     }
@@ -228,22 +155,20 @@ console.log(splitWord)
 var blank = []
 
 // pushes dashes based on the current word length
-    for (i = 0; i < wordChoices[randomNum].length; i++) {
+for (i = 0; i < wordChoices[randomNum].length; i++) {
     blank.push('_')
     // creates blank space between dashes
     document.getElementById("guessLine").textContent = blank.join(' ');
-    }
+}
 
 console.log(currentWord)
 
 // creates a function called "placeLetter" that... ???
-function placeLetter(letterArray){
+function placeLetter(letterArray) {
 
     // creates blank space between dashes
     document.getElementById("guessLine").textContent = letterArray.join(' ');
 }
-
-
 
 // Help from Nicole
 
--- raw.js
+++ raw.js
@@ -1,86 +1,83 @@
 var wordChoices = ["nomadic", "indignant", "disparity", "androgynous", "pragmatic", "patronage", "dilapidated", "putative"]
 
+
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ]
 
+var attempts = 6
 
-// creates for loop to create all of the buttons for the letters
+document.getElementById("chances").innerHTML = attempts;
+
 for (let i = 0; i < alphabet.length; i++) {
-    // creates a ghost div
+
     var div = document.createElement('div');
-    // pulls the individual letter from the array
+
     var letters = document.createTextNode(alphabet[i])
-    // combination of the above 2 lines, allows you to push the text (single letter) into the ghost div... this is similar to using "div.innerHTML = alphabet[i]"
-    div.appendChild(letters);   
-    // adds a classname to the ghost div (so that ALL of the ghost divs can be targeted later)
+    div.appendChild(letters);
+
     div.classList.add('letterBox');
-    // creates a CSS ID for the ghost div (so that individual ghost divs can be targeted later)
-    div.id = "button" + i
-    // targets the HTML body and adds the ghost div to the code (almost like writing HTML in JS; isntead of writing div boxes for each letter, this creates all of the boxes dynamically)
+
+    div.id = "button" + i;
+
     document.body.appendChild(div);
-    // creates a variable called "box" and ?????????
+
     var box = document.getElementById(div.id);
-    // creates functionality so that the letters are clickable
+
     box.addEventListener('click', function() {
+        document.getElementById('button' + i).style.backgroundColor = 'orange';
+        document.getElementById("chances").innerHTML = (attempts--);
+        console.log(alphabet[i])
         checkWord(alphabet[i])
-        document.getElementById("box").style.color = "pink";
-        // functionality not created yet... can be used to show the letter in a "used" box later 
-        // showLetter(i)
-        // functionality not created yet... can be used to hide the letter after it has been clicked on and used
-        // hideLetter(i)
+
     })
 }
 
-
-
-var blank = []
-
-// creates a for loop to create underscores for each letter of the word to be guessed
-for (i = 0; i < wordChoices[randomNum].length; i++) {
-    blank.push('_')
-    // targets the HTML div "guessLine" and adds the underscores from the variable "blank" into div
-    document.getElementById("guessLine").textContent = blank.join(' ');
-}
-
-
-// checks if letter guessed is included in word
 function checkWord(guessedLetter) {
 
     if (currentWord.includes(guessedLetter)) {
-        // gets the index number of the CORRECT letter that was guessed in the word
+
         var n = currentWord.indexOf(guessedLetter);
-        // creates a variable "dashLetter" and gets the HTML element "guessLine" and then pushes the value to the div ID (similar to innerHTML?) 
+
+        console.log(guessedLetter);
+
         var dashLetter = document.getElementById("guessLine").textContent;
-        // creates a variable named "dashArray" that adds dashes between each letter of the word to be guessed
+
+        console.log(dashLetter);
+
         var dashArray = dashLetter.split(" ");
+
+        console.log(dashArray);
 
         dashArray.splice(n, 1, guessedLetter);
 
-        // calls the function placeLetter (see below) and passes the variable "dashArray"
+        console.log(dashArray)
+
         placeLetter(dashArray);
 
     } else {
-
         console.log('incorrect')
     }
 }
 
-// shows word with blank spaces
-function showLetter(currentItem) {
-    
-}
+function showLetter(currentItem) {}
 
 var randomNum = Math.floor(Math.random() * wordChoices.length);
+
 var currentWord = wordChoices[randomNum]
+
 var splitWord = currentWord.split('')
+
 console.log(splitWord)
 
+var blank = []
+for (i = 0; i < wordChoices[randomNum].length; i++) {
+    blank.push('_')
+    document.getElementById("guessLine").textContent = blank.join(' ');
+}
 
+console.log(currentWord)
 
-
-// places letter 
 function placeLetter(letterArray) {
-
     document.getElementById("guessLine").textContent = letterArray.join(' ');
 }
