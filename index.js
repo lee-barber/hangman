57109a551075c275c867b57d254158e3c8c6e7b5
Lee Barber <lfb11@alumni.psu.edu>
2018-02-16 00:49:03 -0500

    functionality finished

--- index.js
+++ index.js
@@ -1,60 +1,11 @@
-// PSEUDO CODE
-
-// # Rules
-// 6 initial attempts to guess word correctly
-// Player wins if he/she guesses word before 6 attemps are used
-// Player loses when attempts == 0
-
-// # Definitions
-// array of words = wordbank
-// array of letters = alphabet
-// attempts remaining = 6
-
-// # Functionality
-// Game starts upon page load
-
-// # Display
-// Rules
-// Blank spaces
-// Alphabet
-// Number of attempts
-
-// As long as (attempts > 0){
-// Game is live...  
-//  Display to user: "Guess a letter"
-//  User guesses letter by clicking letter in displayed alphabet
-//  if (letter exists word){
-//      for each instance of that letter in word{
-//          change blank space to guessed letter
-//          mark letter as used in the alphabet and make it unclickable
-//      }
-//  }
-//  elseif (letter does NOT exist in word){
-//          mark letter as used in the alphabet and make it unclickable
-//          deduct 1 from number of attempts remaining
-//  }
-
-//  if (guessed word == current word from wordbank){
-//      Stop program / exit loop
-//      Display "Game is over. You win."
-//      Display exit options
-//  }
-// }
-// Otherwise{
-//  Stop program / exit loop
-//  Display "Game is over. You lose."
-//  Display exit options
-// }
-
 // variable that contains an array of the game words to choose from 
-var wordChoices = ["nomadic", "indignant", "disparity", "androgynous", "pragmatic", "patronage", "dilapidated"]
+var wordChoices = ["nomad", "indignant", "disparity", "androgynous", "pragmatic", "dilapidated", "putative", "visceral", "extrapolate", "nepotism", "colloquial", "fervently", "auspices", "semantics", "adage", "auspicious", "voila", "culpable", "punitive", "propensity", "abhor", "acumen", "impetus", "dossier", "emphatically", "perseverate", "caveat", "garner", "alchemy", "luddite", "abdicate", "satiate", "nefarious", "insatiable", "percolate", "premonition", "quandary", "aggrieved", "ostensibly", "volition", "impervious", "proclivity", "disillusioned", "tutelage", "innocuous", "indelible", "provocative", "sanctity", "terse", "guise", "auspice", "unabated", "impetuous"]
 
 // hints for each word
-var wordHints = ["hint1", "hint2", "hint3", "hint4", "hint5", "hint6", "hint7", "hint8"]
+var wordHints = ["A member of a people that travels from place to place to find fresh pasture for its animals and has no permanent home.", "Feeling or showing anger or annoyance at what is perceived as unfair treatment.", "A great difference.", "Partly male and partly female in appearance; of indeterminate sex.", "Dealing with things sensibly and realistically in a way that is based on practical rather than theoretical considerations.", "(of a building or object) in a state of disrepair or ruin as a result of age or neglect.", "Generally considered or reputed to be.", "Relating to deep inward feelings rather than to the intellect.", "Extend the application of (a method or conclusion) to an unknown situation by assuming that existing trends will continue or similar methods will be applicable.", "The practice among those with power or influence of favouring relatives or friends, especially by giving them jobs.", "(of language) used in ordinary or familiar conversation; not formal or literary.", "Very enthusiastically or passionately.", "With the help, support, or protection of.", "The branch of linguistics and logic concerned with meaning.", "A proverb or short statement expressing a general truth.", "Conducive to success; favourable.", "There it is; there you are.", "Deserving blame.", "Inflicting or intended as punishment.", "An inclination or natural tendency to behave in a particular way.", "Regard with disgust and hatred.", "The ability to make good judgements and take quick decisions.", "Something that makes a process or activity happen or happen more quickly.", "A collection of documents about a particular person, event, or subject.", "In a forceful way.", "Repeat or prolong an action, thought, or utterance after the stimulus that prompted it has ceased.", "A warning or proviso of specific stipulations, conditions, or limitations.", "Gather or collect (something, especially information or approval)", "A seemingly magical process of transformation, creation, or combination.", "A person opposed to increased industrialization or new technology.", "Fail to fulfil or undertake (a responsibility or duty)", "Supply (someone) with as much as or more of something than is desired or can be managed.", "(typically of an action or activity) wicked or criminal.", "(of an appetite or desire) impossible to satisfy.", "Spread gradually through an area or group of people.", "A strong feeling that something is about to happen, especially something unpleasant.", "A state of perplexity or uncertainty over what to do in a difficult situation.", "Feeling resentment at having been unfairly treated.", "As appears or is stated to be true, though not necessarily so; apparently.", "The faculty or power of using one's will.", "Unable to be affected by (or penetrated)", "A tendency to choose or do something regularly; an inclination or predisposition towards a particular thing.", "Disappointed in someone or something that one discovers to be less good than one had believed.", "Protection of or authority over someone or something; guardianship.", "Not harmful or offensive.", "(of ink or a pen) making marks that cannot be removed. Not able to be forgotten.", "Causing anger or another strong reaction, especially deliberately.", "The state or quality of being holy, sacred, or saintly. Ultimate importance and inviolability.", "Sparing in the use of words; abrupt.", "An external form, appearance, or manner of presentation, typically concealing the true nature of something.", "With the help, support, or protection of.", "Without any reduction in intensity or strength.", "Acting or done quickly and without thought or care."]
 
 // array of each letter in the alphabet to choose from
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 // number of chances remaining displayed to the user
 // document.getElementById("chances").innerHTML = attempts;
@@ -65,9 +16,16 @@ var randomNum = Math.floor(Math.random() * wordChoices.length);
 // assigns the current word to the value of the current word at the random index number 
 var currentWord = wordChoices[randomNum]
 
+// assigns the current word to the value of the current word at the random index number 
+var currentHint = wordHints[randomNum]
+
+// prints the hint for the word to be guessed
+document.getElementById("hints").innerHTML = "<h1>Hint:</h1> <p>   " + currentHint;
+
 // attempts remaining = 6
-var attempts = 6;
-document.getElementById("chances").innerHTML = (attempts);
+var attempts = 6
+// document.getElementById("chances").innerHTML = (attempts);
+document.getElementById("chances").innerHTML = (attempts) + " attempts remaining";
 
 // creates a blank array to have dashes pushed to
 var blank = []
@@ -97,6 +55,8 @@ for (let i = 0; i < alphabet.length; i++) {
     })
 }
 
+// document.getElementById('letterBox').style.margin = 0 auto; ?????
+
 // pushes dashes based on the current word length
 for (i = 0; i < wordChoices[randomNum].length; i++) {
     blank.push('_')
@@ -117,19 +77,15 @@ function checkWord(guessedLetter) {
     }
     if (!bool) {
         attempts--
-        document.getElementById("chances").innerHTML = attempts;
+        document.getElementById("chances").innerHTML = attempts + " attempts remaining";
     }
-    if (currentWord == blank.join('')){
-        alert('You win!')
-    }
-
-    else if (attempts == 0) {
-        alert("You lose")
+    if (currentWord == blank.join('')) {
+        alert('Nice job!  You won!  :)')
+    } else if (attempts == 0) {
+        alert("Sorry, you lost.  :(\n\nThe word was: " + currentWord.toUpperCase())
         location.reload()
     }
 }
 
-
 console.log(currentWord)
-
-
+console.log(currentHint)
